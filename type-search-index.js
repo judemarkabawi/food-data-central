2abb9ab6d5a8311901111973e6bc6fc86329f7e8
judemarkabawi <judemarkabawi@users.noreply.github.com>
2024-11-06 03:08:07 +0000

    deploy: 1874dd09d2b50fafd16b0c68635d10fb507980e2

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fooddatacentral","l":"Food"},{"p":"fooddatacentral","l":"Nutrient"},{"p":"fooddatacentral","l":"NutrientType"},{"p":"fooddatacentral","l":"Units"}];updateSearchResults();
