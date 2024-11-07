c8723ba75e66020fbbad62ea4496c3d4247ce909
Zihe Zhao <zihe.zhao@outlook.com>
2024-11-06 20:57:24 -0500

    adding doc for enum types

--- fooddatacentral/FoodSortableAttributes.java
+++ fooddatacentral/FoodSortableAttributes.java
@@ -4,8 +4,20 @@ package fooddatacentral;
  * Enum representing what attributes of food you can sort by and search by (i.e. columns).
  */
 public enum FoodSortableAttributes {
+    /**
+     * Sortable by the name of the food.
+     */
     NAME,
+    /**
+     * Sortable by the brand of the food.
+     */
     BRAND,
+    /**
+     * Sortable by the serving size of the food.
+     */
     SERVING_SIZE,
+    /**
+     * Sortable by the number of calories in the food.
+     */
     CALORIES
 }
--- fooddatacentral/NutrientType.java
+++ fooddatacentral/NutrientType.java
@@ -4,16 +4,52 @@ package fooddatacentral;
  * Enum representing various types of nutrients that a food item can contain.
  */
 public enum NutrientType {
+    /**
+     * Nutrient type representing carbohydrates.
+     */
     CARBOHYDRATE,
+    /**
+     * Nutrient type representing fat.
+     */
     FAT,
+    /**
+     * Nutrient type representing saturated fat.
+     */
     SATURATED_FAT,
+    /**
+     * Nutrient type representing trans fat.
+     */
     TRANS_FAT,
+    /**
+     * Nutrient type representing cholesterol.
+     */
     CHOLESTEROL,
+    /**
+     * Nutrient type representing fiber.
+     */
     FIBER,
+    /**
+     * Nutrient type representing sugar.
+     */
     SUGARS,
+    /**
+     * Nutrient type representing protein.
+     */
     PROTEIN,
+    /**
+     * Nutrient type representing calcium.
+     */
     CALCIUM,
+    /**
+     * Nutrient type representing iron.
+     */
     IRON,
+    /**
+     * Nutrient type representing potassium.
+     */
     POTASSIUM,
-    CALORIES,
+    /**
+     * Nutrient type representing sodium.
+     */
+    SODIUM,
 }
